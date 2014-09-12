5190ed505cb7e07b3f0148faf6836ef6e7f84c36
DeBraid <de.braid@gmail.com>
2014-09-12 14:05:37 -0400

    loaded more images

--- client/views/common/palimg.js
+++ client/views/common/palimg.js
@@ -1,29 +1,16 @@
+Template.palimg.events({
+  'click #close': function () {
+    $('#palimg .btn-group .open').collapse('hide');
+  }
+});
+
 Template.palimg.helpers({
-  colourPals: function () {
-    // old ones
-      return [
-        { src: 'superagatepal.jpg' , desc : 'Super Agate' },
-        { src: 'standardpal.jpg' , desc : 'Standard' },
-        { src: 'radiantpal.jpg' , desc : 'Radiant' },
-        { src: 'pastelpearlpal.jpg' , desc : 'Pearl' },
-        { src: 'neonpal.jpg' , desc : 'Neon' },
-        { src: 'metallicpal.jpg' , desc : 'Mettallic' },
-        { src: 'jewelpal.jpg' , desc : 'Jewel' },
-        { src: 'adwrappal.jpg' , desc : 'Adwrap' },
-        { src: 'adritemetallicpal.jpg' , desc : 'Adrite Metallic' },
-        { src: 'adritefunpal.jpg' , desc : 'Adrite Fun' },
-        { src: 'adritecrystalpal.jpg' , desc : 'Adrite Crystal' },
-        { src: 'adritebasicpal.jpg' , desc : 'Adrite Basic' },
-        { src: 'outdoorpal.jpg' , desc : 'Outdoor' },
-        { src: 'qlinkpal.jpg' , desc : 'Quick Link' }
-      ]
+  pals: function () {
+      return Session.get('qualatexPals');
   },
   qualatexPals: function () {
       return [
         { src: 'superagatepal.jpg' , desc : 'Super Agate' },
-        { src: 'qlstandardpal.jpg' , desc : 'QuickLink Standard' },
-        { src: 'qljewelpal.jpg' , desc : 'QuickLink Jewel' },
-        { src: 'qlfashionpal.jpg' , desc : 'QuickLink Fashion' },
         { src: 'neonpal.jpg' , desc : 'Neon' },
         { src: 'metallicpal.jpg' , desc : 'Mettallic' },
         { src: 'standardpal.jpg' , desc : 'Standard' },
@@ -33,7 +20,42 @@ Template.palimg.helpers({
         { src: 'fashionpal.jpg' , desc : 'Fashion' }
       ]
   },
-  qualatexPals: function () {
+  qlPals: function () {
+    return [
+        { src: 'qlstandardpal.jpg' , desc : 'QuickLink Standard' },
+        { src: 'qljewelpal.jpg' , desc : 'QuickLink Jewel' },
+        { src: 'qlfashionpal.jpg' , desc : 'QuickLink Fashion' }
+    ]
+  },
+  adritePals: function () {
+   return [
+      { src: 'adrite-asst-basic.jpg' , desc : 'Basic Assortment' },
+      { src: 'adrite-asst-crystal.jpg' , desc : 'Crystal Assortment' },
+      { src: 'adrite-asst-fun.jpg' , desc : 'Fun Assortment' },
+      { src: 'adritebasicpal.jpg' , desc : 'Basic' },
+      { src: 'adritecrystalpal.jpg' , desc : 'Crystal' },
+      { src: 'adritefunpal.jpg' , desc : 'Fun' },
+      { src: 'adritemetallicpal.jpg' , desc : 'Metallic' }
+    ]
+  },
+  cloudbusterPals: function () {
+    return [
+      { src: 'cloudbusterpal.jpg' , desc : 'Cloudbuster' }
+    ]
+  },
+  outdoorPals: function () {
+   return [
+      { src: 'outdoor-asst-autumn.jpg' , desc : 'Autumn Assortment' },
+      { src: 'outdoor-asst-basic.jpg' , desc : 'Basic Assortment' },
+      { src: 'outdoor-asst-crystal.jpg' , desc : 'Crystal Assortment' },
+      { src: 'outdoor-basic.jpg' , desc : 'Basic' },
+      { src: 'outdoor-crystal.jpg' , desc : 'Crystal' },
+      { src: 'outdoor-fun.jpg' , desc : 'Fun' },
+      { src: 'outdoor-metallic.jpg' , desc : 'Metallic' },
+      { src: 'outdoor-tropical.jpg' , desc : 'Tropical' },
+    ]
+  },
+  microfoilPals: function () {
       return [
         { src: 'microfoilblimp.jpg' , desc : 'Blimp' },
         { src: 'microfoildiamond.jpg' , desc : 'Diamond' },
@@ -50,7 +72,7 @@ Template.palimg.helpers({
       ]
   },
   ballPals: function () {
-     return [
+      return [
         { src: 'baseballsoccerpal.jpg' , desc : 'Baseball & Soccer' },
         { src: 'basketballpal.jpg' , desc : 'Basketball' },
         { src: 'footballnostripes.jpg' , desc : 'Football No Stripes 1' },
@@ -61,63 +83,54 @@ Template.palimg.helpers({
   }
 });
 
-Template.palimg.events({
-  'click #close': function () {
-    $('#palimg .btn-group .open').collapse('hide');
-  }
-});
+// Template.outdoorpalsdropdown.helpers({
+//   outdoorPals: function () {
+//    return [
+//       { src: 'outdoor-asst-autumn.jpg' , desc : 'Autumn Assortment' },
+//       { src: 'outdoor-asst-basic.jpg' , desc : 'Basic Assortment' },
+//       { src: 'outdoor-asst-crystal.jpg' , desc : 'Crystal Assortment' },
+//       { src: 'outdoor-basic.jpg' , desc : 'Basic' },
+//       { src: 'outdoor-crystal.jpg' , desc : 'Crystal' },
+//       { src: 'outdoor-fun.jpg' , desc : 'Fun' },
+//       { src: 'outdoor-metallic.jpg' , desc : 'Metallic' },
+//       { src: 'outdoor-tropical.jpg' , desc : 'Tropical' },
+//     ]
+//   }
+// });
 
-Template.outdoorpalsdropdown.helpers({
-  outdoorPals: function () {
-   return [
-      { src: 'outdoor-asst-autumn.jpg' , desc : 'Autumn Asst' },
-      { src: 'outdoor-asst-basic.jpg' , desc : 'Basic Asst' },
-      { src: 'outdoor-asst-crystal.jpg' , desc : 'Crystal Asst' },
-      { src: 'outdoor-basic.jpg' , desc : 'Basic' },
-      { src: 'outdoor-crystal.jpg' , desc : 'Crystal' },
-      { src: 'outdoor-fun.jpg' , desc : 'Fun' },
-      { src: 'outdoor-metallic.jpg' , desc : 'Metallic' },
-      { src: 'outdoor-tropical.jpg' , desc : 'Tropical' },
-    ]
-  }
-});
+// Template.adritepalsdropdown.helpers({
+//   adritePals: function () {
+//    return [
+//       { src: 'adrite-asst-basic.jpg' , desc : 'Basic Assortment' },
+//       { src: 'adrite-asst-crystal.jpg' , desc : 'Crystal Assortment' },
+//       { src: 'adrite-asst-fun.jpg' , desc : 'Fun Assortment' },
+//       { src: 'adritebasicpal.jpg' , desc : 'Basic' },
+//       { src: 'adritecrystalpal.jpg' , desc : 'Crystal' },
+//       { src: 'adritefunpal.jpg' , desc : 'Fun' },
+//       { src: 'adritemetallicpal.jpg' , desc : 'Metallic' }
+//     ]
+//   }
+// });
 
-Template.adritepalsdropdown.helpers({
-  adritePals: function () {
-   return [
-      { src: 'adrite-asst-basic.jpg' , desc : 'Basic Asst' },
-      { src: 'adrite-asst-crystal.jpg' , desc : 'Crystal Asst' },
-      { src: 'adrite-asst-fun.jpg' , desc : 'Fun Asst' },
-      { src: 'adritebasicpal.jpg' , desc : 'Basic' },
-      { src: 'adritecrystalpal.jpg' , desc : 'Crystal' },
-      { src: 'adritefunpal.jpg' , desc : 'Fun' },
-      { src: 'adritemetallicpal.jpg' , desc : 'Metallic' }
-    ]
-  }
-});
+// Template.cloudbusterpalsdropdown.helpers({
+//   cloudbusterPals: function () {
+//     return [
+//       { src: 'cloudbusterpal.jpg' , desc : 'Cloudbuster' }
+//     ]
+//   }
+// });
 
-Template.cloudbusterpalsdropdown.helpers({
-  cloudbusterPals: function () {
-    return [
-      { src: 'cloudbusterpal.jpg' , desc : 'Cloudbuster' }
-    ]
-  }
-});
-
-Template.qualatexpalsdropdown.helpers({
-  qualatexPals: function () {
-      return [
-        { src: 'superagatepal.jpg' , desc : 'Super Agate' },
-        { src: 'qlstandardpal.jpg' , desc : 'QuickLink Standard' },
-        { src: 'qljewelpal.jpg' , desc : 'QuickLink Jewel' },
-        { src: 'qlfashionpal.jpg' , desc : 'QuickLink Fashion' },
-        { src: 'neonpal.jpg' , desc : 'Neon' },
-        { src: 'metallicpal.jpg' , desc : 'Mettallic' },
-        { src: 'standardpal.jpg' , desc : 'Standard' },
-        { src: 'pearlpastelpal.jpg' , desc : 'Pastel Pearl' },
-        { src: 'pearlradiantpal.jpg' , desc : 'Radiant Pearl' },
-        { src: 'jewelpal.jpg' , desc : 'Jewel' },
-        { src: 'fashionpal.jpg' , desc : 'Fashion' }
-      ]
-  }
-});
+// Template.qualatexpalsdropdown.helpers({
+//   qualatexPals: function () {
+//       return [
+//         { src: 'superagatepal.jpg' , desc : 'Super Agate' },
+//         { src: 'neonpal.jpg' , desc : 'Neon' },
+//         { src: 'metallicpal.jpg' , desc : 'Mettallic' },
+//         { src: 'standardpal.jpg' , desc : 'Standard' },
+//         { src: 'pearlpastelpal.jpg' , desc : 'Pastel Pearl' },
+//         { src: 'pearlradiantpal.jpg' , desc : 'Radiant Pearl' },
+//         { src: 'jewelpal.jpg' , desc : 'Jewel' },
+//         { src: 'fashionpal.jpg' , desc : 'Fashion' }
+//       ]
+//   }
+// });

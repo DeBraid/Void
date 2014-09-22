33dbc0f19c19412d21b9a6a0eb93b7cf6b672451
DeBraid <de.braid@gmail.com>
2014-09-22 19:19:31 -0400

    added qualatex logos, footer css and special shapes

--- client/main.js
+++ client/main.js
@@ -24,12 +24,12 @@ Template.contact.rendered = function () {
     }
 };
 
-Template.footer.rendered = function () {
-  var height_diff = $(window).height() - $('body').height();
-    if ( height_diff > 0 ) {
-        $('footer').css( 'margin-top', height_diff );
-    }
-};
+// Template.footer.rendered = function () {
+//   var height_diff = $(window).height() - $('body').height();
+//     if ( height_diff > 0 ) {
+//         $('footer').css( 'margin-top', height_diff );
+//     }
+// };
 
 // pop up when user clicks images
 Template.layout.events({

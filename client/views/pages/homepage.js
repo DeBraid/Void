48bba887597980ddc7c67022faf1b5c3b8ae75a8
DeBraid <de.braid@gmail.com>
2014-10-29 17:20:29 -0400

    homepage caro working dynamic

--- client/views/pages/homepage.js
+++ client/views/pages/homepage.js
@@ -1,11 +1,11 @@
 Template.carousel.helpers({
   carouselItems: function () {
     return [
-      { src : "quicklink-carousel.png" , path : "quicklinkonecolour" },
-      { src : "pioneerbg-crop.png" , path : "promoflags" },
-      { src : "event-tents-carousel.jpg" , path : "eventtents" },
-      { src : "play-balls-carousel.jpg" , path : "admaxminisportballs" },
-      { src : "quicklink-carousel.png" , path : "quicklinkonecolour" }
+      { src : "pioneerbg-crop.png" , path : "promoflags" , klass : "item active" },
+      { src : "quicklink-carousel.png" , path : "quicklinkonecolour" , klass : "item" },
+      { src : "event-tents-carousel.jpg" , path : "eventtents" , klass : "item" },
+      { src : "play-balls-carousel.jpg" , path : "admaxminisportballs" , klass : "item" },
+      { src : "quicklink-carousel.png" , path : "quicklinkonecolour" , klass : "item" }
     ]
   } 
 }); 

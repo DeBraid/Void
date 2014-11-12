0964402210eb52e6f491b45337293974f457b63b
DeBraid <de.braid@gmail.com>
2014-11-11 20:02:51 -0500

    new list of todos after meeting

--- README.md
+++ README.md
@@ -23,19 +23,50 @@ Transform the paper-based product catalogue into digital version.
   a.  Size
   b.  see Catalogue layout
 
-4.  Each Page needs Images
+4.  Each Page needs images
 -- move the table over to say col-7, on left insert the palette or valid img
 
 5.  Homepage design makeover
   a.  set up large images w/ links to sections (duplicate nav, but visually)
-  b.  see pioneer.com for template (also request these images)
+  b.  see pioneer.com for template (also request these images).
+
+***************************
+  NOV 11th
+***************************
+
+9" Standard – Standards,9" Jewel or Fashion – Pierres précieuses ou de mode,9" Metallic – métallisées
+11" Standard – standards,11" Jewel or Fashion – Pierres précieuses ou de mode,11" Metallic, Pearl or Neon – Métallisées, perlées ou néons,16" Standard – Standards
+16" Jewel or Fashion Colours – Pierres précieuses ou de mode,16" Metallic or Pearl – Métallisées ou perlées
+
+
+Notes for Glory: 
+
+1.  no missed lines or double spaces. 
+2.  no punctuation or special characters
 
 
 
 
+******************************************************
+Meeting Notes - Week of Nov 11
+******************************************************
 
-
-
+1.  SEO 
+2.  Include all relevant resources 
+3.  Add Western Canada Phone #
+4.  Layout: Review QuickLink format vs. Standard layout.
+5.  Make click event on table headers for price ONLY
+6.  Create page for Stock Balloons
+7.  French Language Content
+8.  Add Maple Leaf to top-right of all pages
+9.  Get logo as .png with transparent background
+10. IE BUG or palette stretch in diff browsers?  
+11. View Each Balloon individually 
+12. Carousel timer aprox 4000ms
+13. remove title and header from home page
+14. see above, replace with Qualatex and AdRite logos
+15. Contact page: insert main, Qualatex and AdRite logos 
+16. Add CSS effect to nav to show routeActive
 
 
 
--- client/views/pages/promoflags.js
+++ client/views/pages/promoflags.js
@@ -55,37 +55,37 @@ Template.econflags.settings = function () {
         showFilter: true,
         showNavigation: 'never',
         fields: [{
-                    key: 'Product Description',
-                    label: 'Product -- Price Per Piece 4C',
-                    fn: function (str) {
-                        var sortString = ("0000" + parseInt(str, 10)).slice(-4),
-                            unit = 'ft',
-                            size = str.split(unit),
-                            myString = "" + size[0] + " " + unit + " " + size[1] + "";
-                        
-                        var html = "<span sortString=" + sortString + ">" + myString + "</span>";
-                        return Spacebars.SafeString(html);
-                    },
-                    sort: 'ascending'
-                },{
-                    key: 'Printing',  
-                    label: 'Printing'
-                },  {
-                    key: 'Includes',
-                    label: 'Includes'
-                },{
-                    key: '2 to 5',
-                    label: '2 to 5'
-                }, {
-                    key: '6 to 10',
-                    label: '6 to 10'
-                }, {
-                    key: '11 to 25',
-                    label: '11 to 25'
-                }, {
-                    key: '26 to 99',
-                    label: '26 to 99'
-                }
-            ]
+                key: 'Product Description',
+                label: 'Product -- Price Per Piece 4C',
+                fn: function (str) {
+                    var sortString = ("0000" + parseInt(str, 10)).slice(-4),
+                        unit = 'ft',
+                        size = str.split(unit),
+                        myString = "" + size[0] + " " + unit + " " + size[1] + "";
+                    
+                    var html = "<span sortString=" + sortString + ">" + myString + "</span>";
+                    return Spacebars.SafeString(html);
+                },
+                sort: 'ascending'
+            },{
+                key: 'Printing',  
+                label: 'Printing'
+            },  {
+                key: 'Includes',
+                label: 'Includes'
+            },{
+                key: '2 to 5',
+                label: '2 to 5'
+            }, {
+                key: '6 to 10',
+                label: '6 to 10'
+            }, {
+                key: '11 to 25',
+                label: '11 to 25'
+            }, {
+                key: '26 to 99',
+                label: '26 to 99'
+            }
+        ]
     };
 };

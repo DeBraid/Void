4ca08aaa8a75deb4545a7c454147a85f5672c3db
DeBraid <de.braid@gmail.com>
2014-11-14 16:17:50 -0500

    routeactive dicked

--- client/views/common/header.js
+++ client/views/common/header.js
@@ -1,26 +1,42 @@
-Template.nav.helpers({
-  messages: function () {
-    return Messages.find();
-  },
-  isLoggedIn: function () {
-    return !!Meteor.user();
-  }
-})
-
 Template.nav.events({
-
   // Fix for mobile nav bar staying when switching routes
   'click .dropdown-menu, .navbar-collapse a': function () {
-      
     var myNav = $('.navbar-header .navbar-toggle');
-
     if ( myNav.css('display') !='none' ) {
-        
         myNav.trigger( 'click' );
-
     }
   },
   'click #contact-button' : function() {
     $('.collapse.in').collapse('hide');
   },
 });
+
+UI.registerHelper('activeIfTemplateIs', function (template) {
+// var balls = ["AdMax™ Mini Sport Balls","Beachballs","AdPunch™ Balls","Pop Up Banners","Table Covers","Event Tents","promoflags"];
+var latex = ['roundballoons','heartballoons','quicklinkonecolour','adwave','adwrap','adriteeconomyline', 'latexMenu'];
+     var currentRoute = Router.current(),
+     route = currentRoute.lookupTemplate();
+     console.log("route", route);
+     console.log("template", template);
+
+     return template === route && _.contains(latex, route) ? 'active' : '';
+  
+});
+
+// Qualatex® Latex
+//     {item : "One-Colour Print"},
+//     {item : "Heart Balloons"},
+//     {item : "Multi-Colour Print"},
+//     {item : "QuickLinkTM One-Colour"},
+//     {item : "AdWrap® Balloons "},
+//     {item : "AdWave® Balloons"},
+//     {item : "Giant Latex Balloons"},
+// Economy Line
+// {item : "AdRite™ Latex Balloons"},
+// {item : "Cloudbuster Balloons"},
+// {item : "Outdoor Balloons Print"},
+// Qualatex® Microfoil®
+// {item : "Small Quantity Screen Print Balloons"},
+// {item : "Large Quantity Flexo Print Balloons"},
+// Balls
+// return
--- client/views/common/nav/latexMenu.js
+++ client/views/common/nav/latexMenu.js
@@ -1,3 +1,15 @@
+Template.resourcesMenu.helpers({
+  resources: function () {
+    return [
+      { title : "QuickLink Instructions" , href : "http://www.pioneerline.com/downloads/instructions/QL_Instructions.pdf" },
+      { title : "Proper Balloon Inflation" , href : "http://www.pioneerline.com/downloads/13fliers/Proper_Balloon_Inflation_Instructions.pdf" },
+      { title : "AdPunch Instructions" , href : "http://www.pioneerline.com/downloads/13fliers/AdPunch_Ball_Inflation_Instructions.pdf" },
+      { title : "Inflate Vinyl Balloons" , href : "http://www.pioneerline.com/downloads/instructions/inflate_vinyl.pdf" }
+    ] 
+  }
+});
+
+
 // Template.latexMenu.events({
 //   'click #Agates': function (e,t) {
 //     e.preventDefault();
@@ -24,16 +36,6 @@
 //         inp.trigger('keyup');
 //   }
 // });
-Template.resourcesMenu.helpers({
-  resources: function () {
-    return [
-      { title : "QuickLink Instructions" , href : "http://www.pioneerline.com/downloads/instructions/QL_Instructions.pdf" },
-      { title : "Proper Balloon Inflation" , href : "http://www.pioneerline.com/downloads/13fliers/Proper_Balloon_Inflation_Instructions.pdf" },
-      { title : "AdPunch Instructions" , href : "http://www.pioneerline.com/downloads/13fliers/AdPunch_Ball_Inflation_Instructions.pdf" },
-      { title : "Inflate Vinyl Balloons" , href : "http://www.pioneerline.com/downloads/instructions/inflate_vinyl.pdf" }
-    ] 
-  }
-});
 
 
 

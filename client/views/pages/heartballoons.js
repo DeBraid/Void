a6f7523a86a8409c6dbaa2d3e93ca7538a7ddf10
DeBraid <de.braid@gmail.com>
2014-11-27 11:31:34 -0500

    updated the heart ballons and db

--- client/views/pages/heartballoons.js
+++ client/views/pages/heartballoons.js
@@ -4,15 +4,16 @@ Template.heartballoons.settings = function () {
         showNavigation: 'never',
         fields: [{
                     key: 'Product Description',
-                    label: 'Product -- Price Per Balloon 4ABCD',
-                    fn: function (str) {
+                    label: 'Product -- Price Per Balloon 4ABCD -- Description du produit',
+                    fn: function (str, obj) {
                         var sortString = ("0000" + parseInt(str, 10)).slice(-4),
                             unit = 'inch',
                             str1 = str.split(unit),
                             size = str1,
                             myString = "" + size[0] + " " + unit + " " + str1[1] + "";
                         
-                        var html = "<span sortString=" + sortString + ">" + myString + "</span>";
+                        var html = "<p sortString=" + sortString + ">" + myString + "</p>" + 
+                                    "<p>" + obj["Product Description_fr"] + "</p>";
                         return Spacebars.SafeString(html);
                     },
                     sort: 'ascending'
